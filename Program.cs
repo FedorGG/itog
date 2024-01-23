b9139ba912e185c82c308455b44891a0751542c7
Fedor <fedorkolada@gmail.com>
2024-01-23 22:18:56 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк в массиве: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] originalArray = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Введите строку: ");
+            originalArray[i] = Console.ReadLine();
+        }
+
+        
+    }
+
+   
