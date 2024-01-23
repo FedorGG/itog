8c784e21b0a63200eeee6644e67d0587a5e4f42d
Fedor <fedorkolada@gmail.com>
2024-01-23 22:23:30 +0300

    4

--- Program.cs
+++ Program.cs
@@ -33,3 +33,17 @@
                 count++;
             }
         }
+   string[] newArray = new string[count];
+        int index = 0;
+        foreach (string str in originalArray)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index] = str;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
